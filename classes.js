41f6b76d9d74a2e95c90e258265474543f67c69b
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-06-29 15:37:10 +0300

    first commit

---
+++ classes.js
@@ -0,0 +1,24 @@
+/** js is an oop language . 
+ * everything in js is object with it's properties and methods. 
+ * we cerate classes to create an object. 
+ * A class : is like an object constructor or a blueprint for creating objects. 
+ * The class defines attributes and the behavior of the object. 
+ * creating an object from a class is called class instantiation  
+ * 
+ */
+
+// defining a classes : 
+
+class ClassName {
+  // code goes here 
+}
+
+// class instantiation : is creating an object from the class 
+
+class Person { // created person class 
+  // code goes here 
+}
+
+const person = new Person() // created person object ... we instantiate an object from a class 
+console.log (person)
+
