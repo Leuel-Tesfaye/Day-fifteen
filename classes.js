fd16365d5f79a58cc8acc4985b9d46cbcd756241
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-06-30 08:44:49 +0300

    class constructor commit

--- classes.js
+++ classes.js
@@ -22,3 +22,44 @@ class Person { // created person class
 const person = new Person() // created person object ... we instantiate an object from a class 
 console.log (person)
 
+// class constructor is a builtin function which allows as to create a blueprint of our object 
+
+class User{
+  constructor (firstName, lastName){
+    console.log(this)
+    this.firstName = firstName
+    this.lastName = lastName
+  }  
+}
+const user = new User( 'Prince', 'Hope')
+console.log(user)
+
+class User {
+  constructor(email, password){
+    console.log(this)
+    this.email = email
+    this.password = password
+  }
+}
+
+const user1 = new User('tesfayeleuel91@gmail.com', 'password')
+const user2 = new User('tesfayeleuel@gmail.com', 'Password')
+const user3 = new User('zablonGafat@gmail.com', 'password')
+console.log(user1)
+console.log(user2)
+console.log(user3)
+
+// person with more properties 
+
+class Person {
+  constructor (firstName, lastName, age, country, city){
+    console.log(this)
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+  }
+}
+const person2 = new Person ('prince', 'hope', '24','Ethiopia', 'Dessie')
+console.log(person2)
